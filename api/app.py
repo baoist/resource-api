1b500731e8bbbf4b356216055c99e99b0e365154
Brad Olson <iam@brad.io>
2015-05-17 20:33:51 -0700

    started app separation

--- api/app.py
+++ api/app.py
@@ -1,5 +1,36 @@
-from flask import Flask
+from functools import wraps
+from flask import g, request, redirect, url_for, Flask
+from flask.ext.httpauth import HTTPBasicAuth
+from models.user import db, User
+
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+db.init_app(app)
+
+def require_apikey(f):
+    @wraps(f)
+    def require_key(*args, **kwargs):
+        return f(*args, **kwargs)
+    return require_key
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+@app.route("/api/log-in", methods=["POST"])
+def login():
+    auth = request.authorization
+    verified = verify_password(auth.username, auth.password)
+
+    return "foo"
 
 @app.route("/")
 def hello():
---
+++ api/models/user.py
@@ -0,0 +1,45 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from collections import OrderedDict
+from sqlalchemy import Column, types
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
+import config
+
+db = SQLAlchemy()
+
+class DictSerializableMixin(object):
+    def _asdict(self):
+        result = OrderedDict()
+        for key in self.__mapper__.c.keys():
+            result[key] = getattr(self, key)
+        return result
+
+class User(db.Model, DictSerializableMixin):
+    __tablename__ = 'users'
+
+    id = Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    auth_token = db.Column(db.String(32), index=True)
+
+    def __init__(self, name, username):
+        self.name = name
+        self.username = username
+        self.auth_token = self.generate_auth_token
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(config.SECRET_KEY, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(config.SECRET_KEY)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
